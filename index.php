0134c51b595822e6d164bec15d86da9de894c4af
caio-mrb <caiomaxwel@hotmail.com>
2023-04-04 23:56:52 +0100

    Form Validation

--- index.php
+++ index.php
@@ -5,31 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <link href="css/loginpage.css" rel="stylesheet">
-
     <title>Login</title>
 </head>
 
 <body>
-    <img src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
+  <img src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
 
-        <form>
+        <form name="loginForm" onsubmit="return(validation())" method="post">
             <h1>Log In</h1>
             <label for="username">
                 <input type="text" id="username" placeholder="username" name="username">
                 <span>Username</span>
             </label>
-            <div class="fail user">asdasd</div>
+            <em name="emptyUser" class="fail">Username can't be empty!</em>
             <label for="password">
                 <input type="password" id="password" placeholder="Password" name="password">
                 <span>Password</span>
             </label>
-            <div class="fail pass">asdasd</div>
-            <button type="button">LOGIN</button>
+            <em name="emptyPass" class="fail">Password can't be empty!</em>
+            
+            <div name="wrongCredentials" class="faildiv"><em class="fail">Username or Password incorrect!</em></div>
+            <button type="submit">LOGIN</button>
         </form>
-
-
+        
+    <script scr="js/formresubmission.js"></script>
     <script src="js/formvalidation.js"></script>
 
 </body>
--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,4 +1,13 @@
 function validation(){
-    document.getElementById("username").style.display = "none";
-    document.getElementById("password").style.display = "none";
+    if( document.loginForm.username.value == "" ) {
+        document.loginForm.emptyUser.style.display = 'block';
+        document.loginForm.username.focus() ;
+        return false;
+     }
+     if( document.loginForm.password.value == "" ) {
+        document.getElementsById("emptyPass").style.display = 'block';
+        document.loginForm.password.focus() ;
+        return false;
+     }
+     return false;
 }
