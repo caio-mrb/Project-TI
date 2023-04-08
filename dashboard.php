7f99d33d4b5ee9918145b423f4a6e64b50e997c1
root <root@Vinicius>
2023-04-08 01:55:05 +0100

    Dashboard design

--- dashboard.php
+++ dashboard.php
@@ -6,39 +6,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta http-equiv="refresh" content="5">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
+    <link href="css/dbpage.css" rel="stylesheet">
 </head>
 <body>
-    <link href="css/dbpage.css" rel="stylesheet">
-    <img src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
+    <img class ="bg" src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
     <div class="container">
-        <div class="row">
-                <div class="card m-3">
-                    <div class="card-title">
-                        <h1 class="text-center"><strong>Armazém</strong></h1>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="text-center">Dashboard Dinâmico</h5>
-                    </div>
-                </div>
+        <div class="card m-3">
+            <div class="card-title">
+                <h1 class="text-center"><strong>Armazém</strong></h1>
             </div>
-            <div class="col"></div>
+            <div class="card-body">
+              <h5 class="text-center">Dashboard Dinâmico</h5>
+            </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <div class="card m-3 cb1">           
                     <div class="card-header text-center">
-                        <strong><?php echo $nome_temperatura . ": ". $valor_temperatura."ºC" ?></strong>
+                        <strong><?php echo "Alarme de Segurança" . " ON" ?></strong>
                     </div>
                     <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, delectus molestiae est sunt necessitatibus at aliquid earum ab, error doloribus magni, atque eveniet aut voluptatibus incidunt excepturi vero provident corrupti?    
+                        <img src="src/Alarm-on.png" class="image">
                     </div>
-                    <div class="card-footer">
-                        06/04/2023
+                    <div class="card-footer text-center">
+                        <?php echo date("d/m/y") . "   " . date("H:i:s") . " - "; ?>
+                        <a href="#">Historico</a>    
                     </div>
                 </div>               
             </div>
@@ -48,10 +46,11 @@
                         <strong><?php echo $nome_temperatura . ": ". $valor_temperatura."ºC" ?></strong>
                     </div>
                     <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tenetur accusamus corrupti laborum sapiente in est autem mollitia quaerat natus fugit ducimus assumenda quisquam rerum, ratione, labore, quas debitis iusto!    
+                        <img class="image" src="src/humidity-high.png">
                     </div>
-                    <div class="card-footer">
-                        06/04/2023
+                    <div class="card-footer text-center">
+                        <?php echo date("d/m/y") . "   " . date("H:i:s") . " - "; ?>
+                        <a href="#">Historico</a>
                     </div>
                 </div>
             </div>               
@@ -61,10 +60,11 @@
                         <strong><?php echo $nome_temperatura . ": ". $valor_temperatura."ºC" ?></strong>
                     </div>
                     <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odio delectus rerum quia? Consectetur commodi laboriosam quae adipisci deleniti totam nemo? Ipsa exercitationem iusto fuga perferendis saepe quas sit veniam?
+                       <img class="image" src="src/light-on.png">
                     </div>
-                    <div class="card-footer">
-                        06/04/2023
+                    <div class="card-footer text-center">
+                        <?php echo date("d/m/y") . "   " . date("H:i:s") . " - "; ?>
+                        <a href="#">Historico</a>
                     </div>
                 </div>               
             </div> 
