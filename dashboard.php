b1f8c9a21b003b06e13e7e4714c5052dcaa0273c
caio-mrb <caiomaxwel@hotmail.com>
2023-04-09 19:45:39 +0100

    Login form working

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     $valor_temperatura = file_get_contents("src/valor.txt");
     $hora_temperatura = file_get_contents("src/hora.txt");
     $nome_temperatura = file_get_contents("src/nome.txt");
@@ -10,9 +12,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">    
     <link href="css/dbpage.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="src/favicon.svg">
+    <title>Dashboard</title>
 </head>
 <body>
     <img class ="bg" src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
--- index.php
+++ index.php
@@ -1,3 +1,119 @@
+<?php
+
+session_start();
+$users = file('src/users.txt',FILE_IGNORE_NEW_LINES);
+
+
+$request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+
+if ($request_method === 'POST') {
+    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['password'] = $_POST['password'];
+
+    header('Location: index.php', true, 303);
+    exit;
+
+} elseif ($request_method === 'GET') {
+    if(isset($_SESSION['username']))
+    {
+        $username = $_SESSION['username'];
+        unset($_SESSION['username']);
+    }
+    if(isset($_SESSION['password']))
+    {
+        $password = $_SESSION['password'];
+        unset($_SESSION['password']);
+    }
+}
+
+
+/*$file_handle = fopen('src/users.txt', 'r');
+function get_all_lines($file_handle) { 
+    while (!feof($file_handle)) {
+        yield fgets($file_handle);
+    }
+}
+
+$valid_user = false;
+foreach (get_all_lines($file_handle) as $line) {
+
+    if($_POST['password'] == $line && $valid_user == true)
+    {
+        $_SESSION['username'] = $_POST['username'];
+        header('location: dashboard.php');
+    }
+    if($_POST['username'] != $line)
+    {
+        continue;
+    }
+    $valid_user = true;
+}
+fclose($file_handle);*/
+?>
+
+
+<?php
+
+define('FILE_ENCRYPTION_BLOCKS', 10000);
+
+/**
+ * @param  string $source  Path of the unencrypted file
+ * @param  $dest  Path of the encrypted file to created
+ * @param  $key  Encryption key
+ */
+function encryptFile($source, $dest, $key)
+{
+    $cipher = 'aes-256-cbc';
+    $ivLenght = openssl_cipher_iv_length($cipher);
+    $iv = openssl_random_pseudo_bytes($ivLenght);
+
+    $fpSource = fopen($source, 'rb');
+    $fpDest = fopen($dest, 'w');
+
+    fwrite($fpDest, $iv);
+
+    while (! feof($fpSource)) {
+        $plaintext = fread($fpSource, $ivLenght * FILE_ENCRYPTION_BLOCKS);
+        $ciphertext = openssl_encrypt($plaintext, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+        $iv = substr($ciphertext, 0, $ivLenght);
+
+        fwrite($fpDest, $ciphertext);
+    }
+
+    fclose($fpSource);
+    fclose($fpDest);
+}
+
+/**
+ * @param  $source  Path of the encrypted file
+ * @param  $dest  Path of the decrypted file
+ * @param  $key  Encryption key
+ */
+function decryptFile($source, $dest, $key)
+{
+    $cipher = 'aes-256-cbc';
+    $ivLenght = openssl_cipher_iv_length($cipher);
+
+    $fpSource = fopen($source, 'rb');
+    $fpDest = fopen($dest, 'w');
+
+    $iv = fread($fpSource, $ivLenght);
+
+    while (! feof($fpSource)) {
+        $ciphertext = fread($fpSource, $ivLenght * (FILE_ENCRYPTION_BLOCKS + 1));
+        $plaintext = openssl_decrypt($ciphertext, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+        $iv = substr($plaintext, 0, $ivLenght);
+
+        fwrite($fpDest, $plaintext);
+    }
+
+    fclose($fpSource);
+    fclose($fpDest);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,13 +122,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/loginpage.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="src/favicon.svg">
     <title>Login</title>
 </head>
 
 <body>
+
+  
+
     <img src="src/layered-waves-haikei.svg" alt="Pink and Orange Waves">
 
-    <form name="loginForm" action="/Project-TI/dashboard.php" onsubmit="return validation()" method="post">
+    <form name="loginForm" onsubmit="return validation()" method="post">
         <h1>Log In</h1>
         <label id="usernameLabel" for="username">
             <input type="text" id="username" placeholder="username" name="username">
@@ -27,16 +147,30 @@
             <input type="password" id="password" placeholder="Password" name="password">
             <span>Password</span>
         </label>
-            <div id="warningPass" class="warningPass">
+        <div id="warningPass" class="warningPass">
                 <div class="warningIcon"></div>
-                <em name="emptyPass" class="fail">Password can't be empty!</em>
+                <em name="emptyPass" class="fail">Password cant be empty!</em>
             </div>
+        <?php
 
-        <div name="wrongCredentials" class="faildiv"><div class="warningIcon"></div><em class="fail">Username or Password incorrect!</em></div>
+            for($i=0;$i<count($users);$i+=2)
+            {
+                if(strcmp($username,$users[$i]) == 0 &&  strcmp($password,$users[$i+1]) == 0)
+                {
+                    $_SESSION['username'] = $_POST['username'];
+                    header('location: dashboard.php'); 
+                }
+            }
+            
+            if(isset($username))
+            {
+                echo '<div class="faildiv"><div class="warningIcon"></div><em class="fail">Username or Password Incorrect!</em></div>';
+            }
+        
+        ?>
         <button type="submit">LOGIN</button>
     </form>
 
-    <script scr="js/formresubmission.js"></script>
     <script src="js/formvalidation.js"></script>
 
 </body>
